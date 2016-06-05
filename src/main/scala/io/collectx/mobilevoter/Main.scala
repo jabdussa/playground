e94697bd4d4339e3214d9e6c773bf1d6fdea4132
Anwar Abdus-Samad <jabdussa@gmail.com>
2016-06-05 11:31:20 -0400

    updates

--- src/main/scala/io/collectx/mobilevoter/Main.scala
+++
@@ -1,9 +0,0 @@
-package io.collectx.mobilevoter
-
-object Main {
-
-  def main(args: Array[String]): Unit = {
-    akka.Main.main(Array(classOf[Load].getName))
-  }
-
-}
--- src/main/scala/io/collectx/mobilevoter/models/Model.scala
+++
@@ -1,8 +0,0 @@
-package io.collectx.mobilevoter.models
-
-
-case class HtmlMsg(data: String)
-
-case class IpInfo(query: String, country: Option[String], city: Option[String], lat: Option[Double], lon: Option[Double])
-
-case class IpPairSummaryRequest(ip1: String, ip2: String)
